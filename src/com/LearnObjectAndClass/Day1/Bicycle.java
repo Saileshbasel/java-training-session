43c189b2fbc6b200a3410b995ae25a81c2977228
SaileshB <saileshbasel@gmail.com>
2022-02-04 14:50:55 -0800

    Bicycle class added to new Package named Day 1

---
+++ src/com/LearnObjectAndClass/Day1/Bicycle.java
@@ -0,0 +1,17 @@
+package com.LearnObjectAndClass.Day1;
+
+public class Bicycle {
+    /*
+    STATES:
+    color
+    price
+    madeIn
+    brand
+    type
+
+    Behaviours
+    ride
+    brake
+    turn
+     */
+}
